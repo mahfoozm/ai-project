a081938725be8700978abdce238b0c1d95fbea07
mahfoozm <mahfoozm@my.yorku.ca>
2024-02-27 19:33:48 -0500

    back to api

--- api/transcription/transcribe.py
+++ api/transcription/transcribe.py
@@ -1,14 +1,13 @@
 import os
 from django.core.files.storage import default_storage
-import whisper
 from pydub import AudioSegment
 from asgiref.sync import sync_to_async
-
+from openai import OpenAI
 
 from .serializers import FileSerializer
 from .models import File
 
-model = whisper.load_model("base.en")
+client = OpenAI()
 
 class Transcribe:
 
@@ -26,11 +25,18 @@ class Transcribe:
     @sync_to_async
     def transcribe_file(self, file_id):
         file = File.objects.filter(id=file_id).first()
-        if (not file):
+        if not file:
             return None
-        audio_file = Transcribe.get_audio_file(file);
-        transcription = model.transcribe(audio_file)
-        file.transcript = transcription['text'].strip()
+        audio_file_path = default_storage.path(file.file.name)
+        audio_file = open(audio_file_path, 'rb')
+        
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file,
+            response_format="text"
+        )
+
+        file.transcript = transcription
         file.save()
         data = FileSerializer(file).data
         return data
